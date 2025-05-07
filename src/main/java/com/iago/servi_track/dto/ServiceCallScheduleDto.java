c1bc99722e346efa3121bdefda5a022c893d2bcc
iago silva <iago.scandido3@gmail.com>
2025-05-06 22:42:02 -0300

    feat: add duplication validation

--- src/main/java/com/iago/servi_track/dto/ServiceCallScheduleDto.java
+++ src/main/java/com/iago/servi_track/dto/ServiceCallScheduleDto.java
@@ -1,11 +1,14 @@
 package com.iago.servi_track.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record ServiceCallScheduleDto(
 		LocalDate serviceScheduleDate,
+		@JsonFormat(pattern = "HH:mm")
 		LocalTime serviceScheduleHour,
 		String serviceDescription,
 		String clientName,
